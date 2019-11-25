f118fe1e302b060939222cc66a4b56a5802317ee
JoaquimAlfredoRibeiro <joaquimalfredoribeiro@gmail.com>
2019-11-25 11:58:34 +0000

    Mapstruct

---
+++ src/main/java/guru/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.web.model.BeerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDTO beerToBeerDTO(Beer beer);
+
+    Beer beerDTOToBeer(BeerDTO beerDTO);
+}
