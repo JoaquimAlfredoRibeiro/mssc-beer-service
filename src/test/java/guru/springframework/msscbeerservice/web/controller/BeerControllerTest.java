597c449adca5ef249475c0688095a35f3aac54bf
JoaquimAlfredoRibeiro <joaquimalfredoribeiro@gmail.com>
2019-11-28 15:52:59 +0000

    Maven BOM

--- src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -7,17 +7,11 @@ import guru.springframework.msscbeerservice.services.BeerService;
 import guru.springframework.msscbeerservice.web.model.BeerDTO;
 import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.constraints.ConstraintDescriptions;
-import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -25,16 +19,9 @@ import java.util.UUID;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(RestDocumentationExtension.class)
-@AutoConfigureRestDocs(uriScheme = "https", uriHost = "dev.springframework.guru", uriPort = 80)
 @WebMvcTest(BeerController.class)
 //@ComponentScan(basePackages = "guru.springframework.msscbeerservice.web.mappers")
 class BeerControllerTest {
@@ -55,19 +42,7 @@ class BeerControllerTest {
 
         mockMvc.perform(get(BeerController.BASE_URL + "beer/{beerId}", UUID.randomUUID().toString())
                                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(document("v1/beer-get",
-                                pathParameters(parameterWithName("beerId").description("UUID of desired beer to get")),
-                                responseFields(
-                                        fieldWithPath("id").description("Id of Beer"),
-                                        fieldWithPath("version").description("Version number"),
-                                        fieldWithPath("createdDate").description("Date Created"),
-                                        fieldWithPath("lastModifiedDate").description("Date Updated"),
-                                        fieldWithPath("beerName").description("Beer Name"),
-                                        fieldWithPath("beerStyle").description("Beer Style"),
-                                        fieldWithPath("upc").description("UPC of Beer"),
-                                        fieldWithPath("price").description("Price"),
-                                        fieldWithPath("quantityOnHand").description("Quantity On hand"))));
+                .andExpect(status().isOk());
     }
 
     @Test
@@ -75,26 +50,12 @@ class BeerControllerTest {
         BeerDTO beerDTO =  getValidBeerDTO();
         String beerDTOJson = objectMapper.writeValueAsString(beerDTO);
 
-        ConstrainedFields fields = new ConstrainedFields(BeerDTO.class);
-
         given(beerService.saveNewBeer(any())).willReturn(beerDTO);
 
         mockMvc.perform(post(BeerController.BASE_URL + "beer")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(beerDTOJson))
-                .andExpect(status().isCreated())
-                .andDo(document("v1/beer-post",
-                                requestFields(
-                                        fields.withPath("id").ignored(),
-                                        fields.withPath("version").ignored(),
-                                        fields.withPath("createdDate").ignored(),
-                                        fields.withPath("lastModifiedDate").ignored(),
-                                        fields.withPath("beerName").description("Name of the beer"),
-                                        fields.withPath("beerStyle").description("Style of Beer"),
-                                        fields.withPath("upc").description("Beer UPC").attributes(),
-                                        fields.withPath("price").description("Beer Price"),
-                                        fields.withPath("quantityOnHand").ignored()
-                                )));
+                .andExpect(status().isCreated());
     }
 
     @Test
@@ -115,21 +76,5 @@ class BeerControllerTest {
                 .price(new BigDecimal("1.00"))
                 .upc(BeerLoader.BEER_1_UPC)
                 .build();
-
-    }
-
-    private static class ConstrainedFields {
-
-        private final ConstraintDescriptions constraintDescriptions;
-
-        ConstrainedFields(Class<?> input) {
-            this.constraintDescriptions = new ConstraintDescriptions(input);
-        }
-
-        private FieldDescriptor withPath(String path) {
-            return fieldWithPath(path)
-                    .attributes(key("constraints")
-                                        .value(StringUtils.collectionToDelimitedString(this.constraintDescriptions.descriptionsForProperty(path), ". ")));
-        }
     }
 }
